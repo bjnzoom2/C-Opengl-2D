2e1cbd08bb3efe7bf0aee6ad8ca6a1feb8c0c57d
bjnzoom2 <bjnzoom2@gmail.com>
2025-10-27 18:24:44 +0800

    Added camera movement

--- main.cpp
+++ main.cpp
@@ -10,11 +10,11 @@ struct gameData {
 	float timer = 0.2;
 
 	std::vector<Object> objects;
-	double cursorX;
-	double cursorY;
+	glm::dvec2 cursorPos = {};
 };
 
 gl2d::Renderer2D renderer;
+gl2d::Camera camera;
 gameData data;
 
 unsigned int windowWidth = 800;
@@ -27,13 +27,33 @@ bool gameLogic(GLFWwindow *window, float deltatime) {
 	renderer.clearScreen({ 0.0f, 0.0f, 0.0f, 0.0f });
 	data.timer += deltatime;
 
+	glm::vec2 cameraMove = {};
+
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
 		glfwSetWindowShouldClose(window, true);
 	}
 
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
+		cameraMove.y = 1;
+	}
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
+		cameraMove.y = -1;
+	}
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS) {
+		cameraMove.x = 1;
+	}
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS) {
+		cameraMove.x = -1;
+	}
+
+	if (cameraMove.x != 0 || cameraMove.y != 0) {
+		cameraMove = glm::normalize(cameraMove);
+		camera.position += 1000 * deltatime * cameraMove;
+	}
+
 	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS && data.timer > 0.2) {
-		glfwGetCursorPos(window, &data.cursorX, &data.cursorY);
-		Object obj(glm::dvec2(data.cursorX, data.cursorY), 20);
+		glfwGetCursorPos(window, &data.cursorPos.x, &data.cursorPos.y);
+		Object obj(glm::dvec2(data.cursorPos.x + camera.position.x, data.cursorPos.y + camera.position.y), 20);
 		data.objects.push_back(obj);
 		data.timer = 0;
 	}
@@ -89,6 +109,7 @@ int main() {
 
 	gl2d::init();
 	renderer.create();
+	renderer.setCamera(camera);
 
 	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
 
@@ -100,7 +121,9 @@ int main() {
 		deltatime = currentframe - lastframe;
 		lastframe = currentframe;
 
+		renderer.pushCamera(camera);
 		gameLogic(window, deltatime);
+		renderer.popCamera();
 	}
 
 	renderer.cleanup();
